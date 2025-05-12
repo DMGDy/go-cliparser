7b3645b31c3280f782c8e9405109e13c2888e3ae
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-05-12 16:54:10 -0400

    can add sub commands

--- main.go
+++ main.go
@@ -6,21 +6,10 @@ import (
 	"os"
 )
 
-type Command struct {
-	options *[]Option
-	command string // ie 'ble', 'ping', 'ls', 'camera'
-	min_opts int // minimum options to provide
-	max_opts int //
-}
-
-type Option[T any] struct {
-	val_range OverRange
-	name string
-	usage string
-	val T // Subcommand values in single, can support comma seperated field (?)
-	// 0 for the following if such doesnt require bounding values
-	minv int // Assumption: float was not found to be parsed in originial program (if it was it was parsed as a string, see 'ping.py')
-	maxv int // This program will keep [minv, maxv) as integer values
+// treat as C union (change later)
+type Value struct {
+	val_type string
+	val interface{}
 }
 
 // maybe better way ?
@@ -29,48 +18,130 @@ type OverRange struct {
 	upper int
 }
 
-func ParseArgs(c Command) {
-	set := flag.NewFlagSet(c.command, flag.ExitOnError)
-
-	// check if valid values
-	for i, o := range c.options {
-		set.Var(o.val, o.name, o.usage)
-	}
-
-	set.Parse(os.Args[2:])
-
+type Command struct {
+	options []Option
+	name string // ie 'ble', 'ping', 'ls', 'camera'
+	min_opts int // minimum options to provide
+	max_opts int //
 }
 
-func CreateOpt[T any](name string, usage string, vals T, minv int, maxv int) *Option {
+type Option struct {
+	val_range OverRange
+	name string
+	usage string
+	val Value
+	// 0 for the following if such doesnt require bounding values
+	minv int // Assumption: float was not found to be parsed in originial program (if it was it was parsed as a string, see 'ping.py')
+	maxv int // This program will keep [minv, maxv) as integer values
+}
 
-	opt := &Option {
+func BoolVal(b bool) Value {
+	return Value {
+		val_type: "bool",
+		val: b,
+	}
+}
+
+func IntVal(i int) Value {
+	return Value {
+		val_type: "int",
+		val: i,
+	}
+}
+
+func StringVal(s string) Value {
+	return Value {
+		val_type: "string",
+		val: s,
+	}
+}
+
+func Float64Val(f float64) Value {
+	return Value {
+		val_type: "float64",
+		val: f,
+	}
+}
+
+func ParseArgs(c Command) *flag.FlagSet{
+	set := flag.NewFlagSet(c.name, flag.ExitOnError)
+
+
+	// check if valid values
+	fmt.Println(len(c.options))
+	for _, o := range c.options {
+		fmt.Println(o.val.val_type)
+		switch o.val.val_type {
+		case "int":
+			set.Int(o.name, o.val.val.(int), o.usage)
+		case "string":
+			set.String(o.name, o.val.val.(string), o.usage)
+		case "float64":
+			set.Float64(o.name, o.val.val.(float64), o.usage)
+		case "bool":
+			set.Bool(o.name, o.val.val.(bool), o.usage)
+		default:
+			fmt.Fprintf(os.Stderr, "Error parsing arguments!\n")
+			os.Exit(1)
+		}
+	}
+	set.Parse(os.Args[2:])
+
+	return set
+}
+
+func CreateOpts(name string, usage string, val Value, minv int, maxv int) Option {
+
+	return Option {
 		name: name,
 		usage: usage,
-		vals: vals,
+		val: val,
 		minv: minv,
 		maxv: maxv,
 	}
-
-	return opt
 }
 
-func CreateCommand(name string, min_opts int, max_opts int, opts []Option) *Command {
-	command := &Command {
+func CreateCommand(name string, min_opts int, max_opts int, opts []Option) Command {
+
+	return Command {
 		name: name,
 		options: opts,
 		min_opts: min_opts,
 		max_opts: max_opts,
 	}
-
-	return command
 }
 
 func main() {
 	fmt.Println("Hello, World!")
-	opts := make([]*Option, 3)
+	var opts []Option
 
-	opts = append(opts, CreateOpts("scanon", "Turns BLE scan on", false, 0, 0))
+	bvalue := BoolVal(false)
+	opts = append(opts, CreateOpts("scanon", "Turns BLE scan on.", bvalue, 0, 0))
+
+	bvalue = BoolVal(true)
+	opts = append(opts, CreateOpts("scanoff", "Turns BLE scan on.", bvalue, 0, 0))
+
+	ivalue := IntVal(1000800)
+	opts = append(opts, CreateOpts("uuid", "This indicates which BLE process to send this message to.", ivalue, 0, 99999999))
 
 	command := CreateCommand("ble", 1, 1, opts)
 
+	flag_set := ParseArgs(command)
+	scanon := flag_set.Lookup("scanon")
+	
+	if scanon == nil {
+		fmt.Fprintf(os.Stderr,"Error getting flag value\n")
+		os.Exit(1)
+	}
+
+	fmt.Println(scanon.Value)
+
+	uuid := flag_set.Lookup("uuid")
+	
+	if scanon == nil {
+		fmt.Fprintf(os.Stderr,"Error getting flag value\n")
+		os.Exit(1)
+	}
+
+	fmt.Println(uuid.Value)
 }
