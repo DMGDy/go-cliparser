3bc3fdb396d3ea0caada9f0106d0e931efb1b494
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-05-20 17:19:25 -0400

    simple build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+BIN="cli"
+LDFLAGS=""
+#LDFLAGS="-s -w"
+export GOOS linux
+export GOARCH amd64
+stripped=false
+
+for arg in "$@"; do
+    case $arg in
+        "stripped")
+            stripped=true
+            echo here
+            LDFLAGS="-s -w"
+            ;;
+        "deploy")
+            export GOARCH="arm64"
+            ;;
+    esac
+done
+
+
+go build -ldflags "$LDFLAGS" -o $BIN
+
+# strip further if 'upx' command is available
+if [ $stripped = true ];then
+    if $(which upx>/dev/null);then
+        upx --brute $BIN
+    fi
+fi
