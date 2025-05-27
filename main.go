d13d8a04ebcc3b875bfb592e37b98aa2ab66a8dd
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-05-27 17:54:00 -0400

    init mqtt client state

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"bitbucket.resideo.com/276733/grip2-cli/util"
 	"bitbucket.resideo.com/276733/grip2-cli/commands"
+	mqtt "bitbucket.resideo.com/276733/grip2-cli/mqtt-client"
 )
 
 // looking for literal argument of "verbose"
@@ -31,6 +32,14 @@ func consumeVerbose(args *[]string) {
 // consume "--verbose" "-v:" if provided
 func main() {
 
+	// init mqtt client
+	err := mqtt.InitClient()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+
 	commands.RegisterCommands()
 
 	if len(os.Args) < 2 {
@@ -39,10 +48,12 @@ func main() {
 	}
 
 
-	_, err := commands.RunCommand(os.Args[1])
+	_, err = commands.RunCommand(os.Args[1])
+
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
+	mqtt.CloseClient()
 }
