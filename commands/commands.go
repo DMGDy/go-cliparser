8e50ecea75d05da353b290327d88252c4f767023
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-06-04 10:35:56 -0400

    add bypass and getinfo; return error to main if running command fails

--- commands/commands.go
+++ commands/commands.go
@@ -10,20 +10,22 @@ import (
 	"bitbucket.resideo.com/276733/grip2-cli/commands/ble"
 	"bitbucket.resideo.com/276733/grip2-cli/commands/armdisarm"
 	"bitbucket.resideo.com/276733/grip2-cli/commands/bypass"
+	"bitbucket.resideo.com/276733/grip2-cli/commands/getinfo"
 )
 
 var Commands = map[string]run.RunCommand {
 	"ble": &ble.Ble{},
 	"armdisarm": &armdisarm.Armdisarm{},
 	"bypass": &bypass.Bypass{},
+	"getinfo": &getinfo.Getinfo{},
 }
 
 func processCommand(c string, r run.RunCommand) {
-		af := util.ArgMap[c]
-		util.ValidateValues(af)
+	af := util.ArgMap[c]
+	util.ValidateValues(af)
 
-		// parse and validate
-		af.FlagSet.Parse(os.Args[2:])
+	// parse and validate
+	af.FlagSet.Parse(os.Args[2:])
 }
 
 func RegisterCommands() {
@@ -33,15 +35,18 @@ func RegisterCommands() {
 }
 
 // either return the response (can be empty or an error)
-func RunCommand(command string) (string, error) {
+func RunCommand(command string) error {
 	r, ok := Commands[command]
 	if ok {
 		processCommand(command, r)
-		r.Run()
+		err := r.Run()
+		if err != nil {
+			return err
+		}
 	} else {
-		return " ", errors.New(fmt.Sprintf("Command `%s` does not exist\n"))
+		return errors.New(fmt.Sprintf("Command `%s` does not exist\n", command))
 	}
 
-	return "", errors.New("TODO")
+	return nil
 }
 
