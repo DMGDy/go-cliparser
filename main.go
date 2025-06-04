0daeb77fda56c1878f31b0e1a784a5a8e3a47ebe
Dylan Dy <dylanmatthew.garza@resideo.com>
2025-06-04 10:37:04 -0400

    consume -v and --ip= if present and assign to global statics

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strings"
 
 	"bitbucket.resideo.com/276733/grip2-cli/util"
 	"bitbucket.resideo.com/276733/grip2-cli/commands"
@@ -11,26 +12,55 @@ import (
 )
 
 // looking for literal argument of "verbose"
-var verbose = false
 
-func consumeVerbose(args *[]string) {
-	n := -1
-	a := slices.Index(*args, "--verbose")
-	b := slices.Index(*args, "-v")
-	if a > b {
-		n = a
-	} else {
-		n = b
+func consumeConfFlags(args *[]string) {
+	a := slices.Index(*args, "-v")
+	var b = 0 
+	for i, s := range *args {
+		if strings.Contains(s, "--ip=") {
+			b = i
+		}
 	}
-	if n != -1 {
-		// Essentially "popping" element from slice
-		*args = slices.Delete(*args, n, n+1)
+
+	if a > 0 {
+		util.Verbose = true
+		*args = slices.Delete(*args, a, a+1)
 		fmt.Printf("**Verbose output enbabled**\n\n")
+	}
+
+	if b > 0 {
+		parts := strings.Split((*args)[b], "=")
+		if len(parts) < 2 {
+			fmt.Println("Argument not provided correctly.")
+			fmt.Println("--ip=[IP]")
+			fmt.Println("ie.) --ip=10.1.195.139")
+			os.Exit(1)
+		}
+		mqtt.IP = parts[1]
+
+		*args = slices.Delete(*args, b, b+1)
+		fmt.Printf("**Using IP: %s**\n\n" ,mqtt.IP)
 	}
 }
 
 // consume "--verbose" "-v:" if provided
 func main() {
+
+	if len(os.Args) < 2 {
+		util.PrintHelpFull()
+		os.Exit(1)
+	}
+
+	consumeConfFlags(&os.Args)
+
+
+	if len(os.Args) < 2 {
+		util.PrintHelpFull()
+		os.Exit(1)
+	}
+
+	// register the defined commands/subcommands to flag module
+	commands.RegisterCommands()
 
 	// init mqtt client
 	err := mqtt.InitClient()
@@ -40,15 +70,9 @@ func main() {
 	}
 
 
-	commands.RegisterCommands()
 
-	if len(os.Args) < 2 {
-		util.PrintHelpFull()
-		os.Exit(1)
-	}
+	err = commands.RunCommand(os.Args[1])
 
-
-	_, err = commands.RunCommand(os.Args[1])
 
 	if err != nil {
 		fmt.Println(err.Error())
